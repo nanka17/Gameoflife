3425772963b015eb3116ece043c08a8f715d642d
Your Name <Your Name>
2021-07-07 17:45:04 +0400

    ok

--- Demon.js
+++ Demon.js
@@ -1,12 +1,8 @@
-
-class Demon {
+class Demon extends LivingCreature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x, y)
         this.energy = 5;
-        this.directions = [];
-        this.multiply = 0
-        this.directions = []
+
     }
     getNewCoordinates() {
         this.directions = [
@@ -23,18 +19,8 @@ class Demon {
 
     chooseCell(character) {
         this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
+        return super.chooseCell(character)
 
-        }
-        return found;
     }
     mul() {
         this.multiply++;
@@ -54,7 +40,7 @@ class Demon {
     }
 
     move() {
-   
+
         var emptyCells = this.chooseCell(0);
         var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
         if (newCell && this.energy > 0) {
@@ -102,7 +88,7 @@ class Demon {
         else {
             this.move()
         }
- 
+
 
     }
     die() {
